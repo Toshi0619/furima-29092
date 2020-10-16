4f877baffbedd8861bd966c7b3175d89ce5e2207
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2020-10-16 18:11:55 +0900

    正規表現を変数へ格納

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :birthday, presence: true
+  
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'include both letters and numbers' }
 
+  NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  KANA_REGEX = /\A[ァ-ヶー－]+\z/
+
   with_options presence: true do
-    validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid.Input full-width characters.' }
-    validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid.Input full-width characters.' }
-    validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
-    validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :firstname, format: { with: NAME_REGEX, message: 'is invalid.Input full-width characters.' }
+    validates :lastname, format: { with: NAME_REGEX, message: 'is invalid.Input full-width characters.' }
+    validates :firstname_kana, format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :lastname_kana, format: { with: KANA_REGEX, message: 'is invalid. Input full-width katakana characters.' }
+    validates :nickname
+    validates :birthday
   end
 end
