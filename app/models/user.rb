9df72d23737ab24701b8f470cd95bb8cc0f72307
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2020-10-16 17:51:32 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   validates :birthday, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "include both letters and numbers"}
 
   with_options presence: true do
     
@@ -13,7 +14,6 @@ class User < ApplicationRecord
     validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid.Input full-width characters."}
     validates :firstname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
     validates :lastname_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "include both letters and numbers"}
  
   end
 
