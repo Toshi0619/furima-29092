462dea84cdecaa51fe4a933e90d3abfbe9255429
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2020-10-24 14:48:24 +0900

    rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,8 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index]
 
   def index
-    @items = Item.all 
+    @items = Item.all
     # Itemテーブルから一覧データを取得
-    
   end
 
   def new
