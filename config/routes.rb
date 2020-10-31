5a369f0809f04d5ab20f79979b88eb279d697a4f
Toshi0619 <chiaki.afro.7.pf@gmail.com>
2020-10-31 20:53:51 +0900

    商品購入画面修正・遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index' #rootパスをindex画面に設定
-  resources :items
+  resources :items do
+    resources :orders
+  end
   # get 'items/new/:id', to: 'items#calculation'
 end
